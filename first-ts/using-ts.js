bc6a2153a5bbed867606dac0410c28e0e33c9a05
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-05-03 20:12:23 +0200

    changed the gitignore

--- first-ts/using-ts.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1"); // means that it will always find this element and specify what it will be
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
-    return num1 + num2;
-}
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value)); //value is always a string, "+" in front of string change it to number
-});
