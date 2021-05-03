94910cbc1883c86ed82a34833243f59b605fe276
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-05-03 19:40:04 +0200

    intro

---
+++ using-ts.js
@@ -0,0 +1,9 @@
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1"); // means that it will always find this element and specify what it will be
+var input2 = document.getElementById("num2");
+function add(num1, num2) {
+    return num1 + num2;
+}
+button.addEventListener("click", function () {
+    console.log(add(+input1.value, +input2.value)); //value is always a string
+});
---
+++ using-ts.ts
@@ -0,0 +1,11 @@
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement; // means that it will always find this element and specify what it will be
+const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+button.addEventListener("click", function () {
+  console.log(add(+input1.value, +input2.value)); //value is always a string
+});
