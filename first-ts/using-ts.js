51d84849613504779cc1a715204e7f738632e61b
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-05-03 20:10:16 +0200

    changed the gitignore

--- first-ts/using-ts.js
+++ first-ts/using-ts.js
@@ -5,5 +5,5 @@ function add(num1, num2) {
     return num1 + num2;
 }
 button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value)); //value is always a string
+    console.log(add(+input1.value, +input2.value)); //value is always a string, "+" in front of string change it to number
 });
